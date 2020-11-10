1a31998dc165f8334119d9fa97f4004ec1d5d0c9
ddominguezh <diegoalbert500@gmail.com>
2020-11-10 22:39:43 +0100

    Update Color.java

--- src/v5/exercise/enums/Color.java
+++ src/v5/exercise/enums/Color.java
@@ -2,17 +2,17 @@ package v5.exercise.enums;
 
 public enum Color {
 	RED('r'),
-    BLUE('b'),
-    GREEN('g'),
-    YELLOW('y'),
-    PINK('p'),
-    MAGENTE('m'),
-    ORANGE('o'),
-    SILVER('s');
-    private char shortName;
-    Color(char shortName) {
+    	BLUE('b'),
+    	GREEN('g'),
+    	YELLOW('y'),
+    	PINK('p'),
+    	MAGENTE('m'),
+    	ORANGE('o'),
+    	SILVER('s');
+    	private char shortName;
+    	Color(char shortName) {
     		this.shortName = shortName;
-    }
+    	}
 	public static Color valueOf(char value) {
 		for(Color color : Color.values()) {
 			if(color.shortName == value) {
